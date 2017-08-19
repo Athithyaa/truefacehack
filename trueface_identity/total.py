055afaaca12819d6d94e494a87d7b7b7695ce245
athithyaa <atpa2251@colorado.edu>
2017-08-19 17:45:48 -0600

    adding new changes

--- trueface_identity/total.py
+++ trueface_identity/total.py
@@ -2,115 +2,69 @@
 import requests
 import base64
 import json
-import time
+import csv
 
-#Creating a colelction
+# Creating a colelction
 headers = {
-  "x-api-key":"k9smH5hhw3J2joOc6cIv8TO5t8iudeI3llnr34D2",
-  "Content-Type":"application/json",
+    "x-api-key": "k9smH5hhw3J2joOc6cIv8TO5t8iudeI3llnr34D2",
+    "Content-Type": "application/json",
 }
 
 url = "https://api.chui.ai/v1/collection"
 
 data = {
-  "name":"Celebrities",
-  "unknowns":False
+    "name": "Celebrities",
+    "unknowns": "false"
 }
 
-r  = requests.post(url,data=json.dumps(data),headers=headers)
+r = requests.post(url, data=json.dumps(data), headers=headers)
 
 print r.content
 
 temp = json.loads(r.content)
 collection_id = str(temp['data']['collection_id'])
 
-#Adding the first profile
-url = "https://api.chui.ai/v1/enroll"
-
-data = {
-  "img0":base64.b64encode(open('0.jpg','rb').read()),
-  "img1":base64.b64encode(open('1.jpg','rb').read()),
-  "img2":base64.b64encode(open('2.jpg','rb').read()),
-  "img3":base64.b64encode(open('3.jpg','rb').read()),
-  "name":"Emilia Clarke",
-  #"collection_id":collection_id
-
+# Adding the first profile
+headers = {
+    "x-api-key": "k9smH5hhw3J2joOc6cIv8TO5t8iudeI3llnr34D2",
+    "Content-Type": "application/json",
 }
 
-r1  = requests.post(url,data=json.dumps(data),headers=headers)
-r1 = r1.json()
-print r1
+enroll_url = "https://api.chui.ai/v1/enroll"
+update_collection_url = "https://api.chui.ai/v1/collection"
+count = 0
 
-# Adding the Second profile
-
-data = {
-  "img0":base64.b64encode(open('01.png','rb').read()),
-  "img1":base64.b64encode(open('11.jpg','rb').read()),
-  "img2":base64.b64encode(open('13.jpg','rb').read()),
-  "name":"Lord of the Rings Lady",
-}
-
-r2  = requests.post(url,data=json.dumps(data),headers=headers)
-r2 = r2.json()
-
-
-
-#update enrollment with collections
-
-
-url = "https://api.chui.ai/v1/collection"
-
-
-#profile 1
-data = {
-  "enrollment_id":r1['data']['enrollment_id'],
-  "collection_id":collection_id,
-
-}
-
-r  = requests.put(url,data=json.dumps(data),headers=headers)
-
-
-print r.text
-
-
-#profile 2
-
-data = {
-  "enrollment_id":r2['data']['enrollment_id'],
-  "collection_id":collection_id,
-
-}
-
-r  = requests.put(url,data=json.dumps(data),headers=headers)
-
-print r.text
-
-
-############################################
-
-
-url = "https://api.chui.ai/v1/train"
-
-data = {
-  "collection_id":collection_id
-}
-
-r  = requests.post(url,data=json.dumps(data),headers=headers)
-
-print r.json()
-
-time.sleep(15)
-
+with open('../data/wiki.csv', 'rb') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    for row in csvreader:
+        filename = '../data/wiki/'
+        image_data = {
+            "img0": base64.b64encode(open(filename + row[1], 'rb').read()),
+            "name": row[0]
+        }
+        enroll_response = requests.post(enroll_url, data=json.dumps(image_data), headers=headers)
+        enroll_response = enroll_response.json()
+        print enroll_response
+        if enroll_response['message'] != u'No face detected !':
+            update_collection_data = {
+                "enrollment_id": enroll_response['data']['enrollment_id'],
+                "collection_id": collection_id,
+            }
+        collection_response = requests.put(update_collection_url, data=json.dumps(update_collection_data),
+                                           headers=headers)
+        print collection_response.json()
+        if count == 100:
+            break
+        count += 1
 
 
 url = "https://api.chui.ai/v1/identify"
 
 data = {
-  "img":base64.b64encode(open('face1.png','rb').read()),
-  "collection_id":collection_id
+    "img": base64.b64encode(open('mrauch.jpg', 'rb').read()),
+    "collection_id": collection_id
 }
 
-r  = requests.post(url,data=json.dumps(data),headers=headers)
+r = requests.post(url, data=json.dumps(data), headers=headers)
 
 print r.json()
