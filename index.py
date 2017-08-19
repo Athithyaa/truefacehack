daff7b52556bb4aeeabd755e1344435e904fef17
athithyaa <atpa2251@colorado.edu>
2017-08-19 11:58:57 -0600

    adding gitignore and frames folder

--- index.py
+++ index.py
@@ -1,10 +1,11 @@
 import cv2
-vidcap = cv2.VideoCapture('big_buck_bunny_720p_5mb.mp4')
-success,image = vidcap.read()
+
+vidcap = cv2.VideoCapture('data/big_buck_bunny_720p_5mb.mp4')
+success, image = vidcap.read()
 count = 0
 success = True
 while success:
-  success,image = vidcap.read()
-  print('Read a new frame: ', success)
-  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
-  count += 1
+    success, image = vidcap.read()
+    print('Read a new frame: ', success)
+    cv2.imwrite("frames/frame%d.jpg" % count, image)  # save frame as JPEG file
+    count += 1
