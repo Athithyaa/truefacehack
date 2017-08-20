f5f0153a8cb57653bc62815441f7776b730bd8a3
Abhijit Suresh <abhijit5893@gmail.com>
2017-08-19 22:02:55 -0600

    Added Train and Video Read

--- video_read.py
+++ video_read.py
@@ -2,6 +2,8 @@ from pytube import YouTube
 import os
 
 yt = YouTube("https://www.youtube.com/watch?v=cqgi1IjDoU8")
+framesPersecond = 60
+
 yt.set_filename('vid1')
 size = len(yt.get_videos())
 if size > 1:
@@ -25,7 +27,8 @@ success = True
 while success:
     success, image = vidcap.read()
     print('Read a new frame: ', success)
-    cv2.imwrite("tmp/frame%d.jpg" % count, image)  # save frame as JPEG file
+    if count % framesPersecond == 0:
+    	cv2.imwrite("tmp/frame%d.jpg" % count, image)  # save frame as JPEG file
     count += 1
 
 import requests
@@ -43,12 +46,14 @@ headers = {
 
 url = "https://api.chui.ai/v1/identify"
 
-for i  in range(count):
-	if i == 2:
-		break
-    	else:
+temp_index = []
+for i in range(count):
+	if i % framesPersecond == 0:
+		temp_index.append(i)
+
+for i  in range(temp_index):
 		data = {
-		    "img":base64.b64encode(open('tmp/frame201.jpg','rb').read()),
+		    "img":base64.b64encode(open('tmp/frame'+str(i)+'.jpg','rb').read()),
 		    "collection_id": "ahBzfmNodWlzcGRldGVjdG9ychcLEgpDb2xsZWN0aW9uGICAgMDjsKkJDA"
 		}
 
